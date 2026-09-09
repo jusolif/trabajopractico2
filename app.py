50b4b6002ac0300da8cbefe2e555e2004d73ac62
jusolif <juliosolis77@gmail.com>
2026-09-09 14:48:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ import streamlit as st
 modulo = st.sidebar.selectbox("Seleccione una sección:",["Home","Ejercicio 1","Ejercicio 2","Ejercicio 3","Ejercicio 4"])
 
 if modulo == "Home":
-  st.title("Proyecto Aplicado en Streamlit – Fundamentos de Programación ")
+  st.title("Primer Proyecto de Portafolio Profesional")
   st.image("DMC.png",width=150)
   st.image("Python_logo.png",width=300)
   st.subheader("Julio Humberto Solis Flores")
-  st.markdown("Módulo 1: Python Fundamentals")
-  st.write("Ingeniero Industrial con experiencia en Business Intelligence")
+  st.markdown("Especialización en Python for Analytics")
   st.write("2026")
-  st.write("Trabajo práctico número 1 para el curso de Especialización en Python for Analytics")
-  st.write("Para este trabajo se usaron tecnologías como Google Colab, Python, GitHub y Streamlit")
+  st.write("El presente proyecto está basado en el archivo el archivo BankMarketing.csv correspondiente a una institución financiera que busca entender
+  los factores que influyen en la aceptación de sus campañas de marketing.")
+  st.write("Para este trabajo se usaron tecnologías como Python, Pandas y Streamlit")
