d15fe61441920539c48a8a55d02e6ab675204691
jusolif <juliosolis77@gmail.com>
2026-09-09 14:58:42 -0500

    Update project description in app.py

--- app.py
+++ app.py
@@ -9,5 +9,5 @@ if modulo == "Home":
   st.subheader("Julio Humberto Solis Flores")
   st.markdown("Especialización en Python for Analytics")
   st.write("2026")
-  st.write("Este proyecto está baasado en el archivo BankMarketing.csv, ")
+  st.write("Este proyecto está baasado en el archivo BankMarketing.csv, correspondiente a una institución financiera que buscan entender los factores que influyen en la aceptación de sus campañas de marketing")
   st.write("Para este trabajo se usaron tecnologías como Python, Pandas y Streamlit")
