7f70d933b2f34afbda48b87e1089e676ef8a7df3
jusolif <juliosolis77@gmail.com>
2026-09-09 14:32:56 -0500

    Implement project details display in Streamlit app
    Added Streamlit components to display project information.

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 import streamlit as st
+if modulo == "Home":
+  st.title("Proyecto Aplicado en Streamlit – Fundamentos de Programación ")
+  st.image("DMC.png",width=150)
+  st.image("Python_logo.png",width=300)
+  st.subheader("Julio Humberto Solis Flores")
+  st.markdown("Módulo 1: Python Fundamentals")
+  st.write("Ingeniero Industrial con experiencia en Business Intelligence")
+  st.write("2026")
+  st.write("Trabajo práctico número 1 para el curso de Especialización en Python for Analytics")
+  st.write("Para este trabajo se usaron tecnologías como Google Colab, Python, GitHub y Streamlit")
