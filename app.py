8063ab4b9dfaf1133ee1d145038dbc64622b5b15
jusolif <juliosolis77@gmail.com>
2026-09-09 14:57:03 -0500

    Refine project description in app.py
    Updated project description for clarity.

--- app.py
+++ app.py
@@ -9,5 +9,6 @@ if modulo == "Home":
   st.subheader("Julio Humberto Solis Flores")
   st.markdown("Especialización en Python for Analytics")
   st.write("2026")
-  st.write("Este proyecto está baasado en el archivo BankMarketing.csv,")
+  st.write("Este proyecto está baasado en el archivo BankMarketing.csv, correspondiente a una institución
+financiera que busca entender los factores")
   st.write("Para este trabajo se usaron tecnologías como Python, Pandas y Streamlit")
